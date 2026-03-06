d2bacc67c245540c46dd3cc4ac6439588d990aff
oliversum-ch <75724465+oliversum-ch@users.noreply.github.com>
2026-03-06 14:12:59 +0100

    Add compatible migrations for upstream 0255 and equipment location

--- migrations/0256.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE equipment ADD location_id INTEGER;
---
+++ migrations/0257.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is executed inside DatabaseMigrationService class/context
+
+$db = $this->getDatabaseService();
+
+$locationColumnExists = $db->ExecuteDbQuery("SELECT COUNT(*) FROM pragma_table_info('equipment') WHERE name = 'location_id'")->fetchColumn() > 0;
+
+if (!$locationColumnExists)
+{
+	$db->ExecuteDbStatement('ALTER TABLE equipment ADD location_id INTEGER;');
+}
