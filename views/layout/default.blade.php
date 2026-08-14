1ca6a54016262fa38b49e3a09adc06bc2636f31b
oliversum-ch <75724465+oliversum-ch@users.noreply.github.com>
2026-08-14 13:16:04 +0200

    Refresh receipt importer browser cache

--- views/layout/default.blade.php
+++ views/layout/default.blade.php
@@ -781,7 +781,7 @@
 
 	@stack('pageScripts')
 	@stack('componentScripts')
-	<script src="{{ $U('/viewjs/' . $viewName . '.js?v=', true) }}{{ $version }}{{ $viewName === 'receiptimport' ? '-generic-receipts-13-debug' : ($viewName === 'productform' ? '-receipt-product-handoff-1' : '') }}"></script>
+	<script src="{{ $U('/viewjs/' . $viewName . '.js?v=', true) }}{{ $version }}{{ $viewName === 'receiptimport' ? '-paddleocr-1' : ($viewName === 'productform' ? '-receipt-product-handoff-1' : '') }}"></script>
 
 	@if(file_exists(GROCY_DATAPATH . '/custom_js.html'))
 	@php include GROCY_DATAPATH . '/custom_js.html' @endphp
